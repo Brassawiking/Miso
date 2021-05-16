394f067e5f58c559f9d8255f75ce9192ed91b3c5
Brassawiking <brassawiking@gmail.com>
2021-05-16 10:07:44 +0200

    Github pages attempt

--- src/www/js/logic/loops/MainGame.js
+++ src/www/js/logic/loops/MainGame.js
@@ -23,7 +23,7 @@ export async function createLoop_MainGame ({
 
   const loadData = async url => (await fetch(url)).json()
   const loadedLands = await Promise.all([
-    loadData('data/miso_land_-1_0.json')
+    loadData('data/welcome.json')
   ])
   loadedLands.forEach(data => {
     const land = LAND.identity(LAND_SIZE)
