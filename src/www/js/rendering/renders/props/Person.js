498faecc1677dd755475fdd64d6542130459ca4e
Brassawiking <brassawiking@gmail.com>
2021-05-23 11:02:07 +0200

    Tweaked person model

--- src/www/js/rendering/renders/props/Person.js
+++ src/www/js/rendering/renders/props/Person.js
@@ -5,6 +5,7 @@ export function createRender_Person(gl, clothColor = [1, 0, 0, 1]) {
   const cloth = 0.6
   const neck = 1.8
   const face = 0.5
+  const footing = -0.05
 
   const mesh = [
     0, leg, -cloth/3,
@@ -36,11 +37,11 @@ export function createRender_Person(gl, clothColor = [1, 0, 0, 1]) {
     face/2, neck+face, 0,
 
     0, leg, cloth/2,
-    -cloth/4, -leg, cloth/3,
+    -cloth/4, footing, cloth/3,
     -cloth/2, leg, cloth/3,
 
     0, leg, cloth/2,
-    cloth/4, -leg, cloth/3,
+    cloth/4, footing, cloth/3,
     cloth/2, leg, cloth/3,
   ]
 
