12faf4ebcf54deeb4c6acea43ca2db41b3bf0988
Brassawiking <brassawiking@gmail.com>
2021-05-29 10:09:47 +0200

    Update changeLog.js

--- src/www/js/changeLog.js
+++ src/www/js/changeLog.js
@@ -1,18 +1,18 @@
 export const changeLog = `
 
 🗓️ May 29, 2021
-🔹 Fixed bug where with movement slowed down at certain camera angles 🐛
+🐛 Fixed bug where with movement slowed down at certain camera angles
 🔹 Code cleanup, some bugs might pop up afterwards 
 
 🗓️ May 28, 2021
 🔹 Added prop STEPS and made an example of them in the intro land 
-🔹 Fifth land created by another person added! ⭐ 
-🔹 Fourth land created by another person added! ⭐ 
+⭐ Fifth land created by another person added!  
+⭐ Fourth land created by another person added! 
 
 🗓️ May 27, 2021
 🔹 Added prop HOUSE_WALL and HOUSE_ROOF 
 🔹 Added some fresh crystals to intro land 
-🔹 Fixed bug with brush did not render under water 🐛
+🐛 Fixed bug with brush did not render under water
 🔹 Added prop CRYSTAL
 🔹 Added prop FENCE and added it to the introduction land
 🔹 Changed background color when transitioning into the game a bit more pleasing
@@ -22,15 +22,15 @@ export const changeLog = `
 
 🗓️ May 25, 2021
 🔹 Added a new mouse action to even out lands
-🔹 Fixed a bug crash when changing land/prop types with keyboard 🐛
+🐛 Fixed a bug crash when changing land/prop types with keyboard
 🔹 Added some error handling so the game does not crash completely at any errors
 
 🗓️ May 24, 2021
-🔹 Added support for post-processing and made an edge silhouette effect with it 🚀
+🚀 Added support for post-processing and made an edge silhouette effect with it
 
 🗓️ May 23, 2021
 🔹 Massive code cleanup, some bugs might pop up from this 
-🔹 Fixed a bug where jumping/falling was affected by the slope below 🐛 
+🐛 Fixed a bug where jumping/falling was affected by the slope below
 🔹 Improved slope handling and added a debug mode 
 🔹 Steep surfaces can no longer be climbed in gravity mode 
 🔹 Change of plan, player avatar now looks in the direction it is moving 
@@ -39,23 +39,23 @@ export const changeLog = `
 
 🗓️ May 22, 2021
 🔹 Added a greeting message when you enter a land 
-🔹 Third land created by another person added! ⭐ 
+⭐ Third land created by another person added! 
 🔹 Improved seam handling 
 🔹 Performance improvements 
 
 🗓️ May 21, 2021
 🔹 Added a soft brush mode (default on) for those smooth rolling hills 
 🔹 Changed player starting position to be more within the intro land 
-🔹 Lands are now stitched for a more seamless experience ⭐ 
+⭐ Lands are now stitched for a more seamless experience 
 🔹 Made the water a bit more fancy with some reflective sunlight 
 
 🗓️ May 20, 2021
-🔹 Second land created by another person added! ⭐ 
+⭐ Second land created by another person added!
 
 🗓️ May 19, 2021
 🔹 Can now load saved land files 
 🔹 Changed fly up/down controls as CTRL+W closes the browser tab 
-🔹 First land created by another person added! ⭐ 
+⭐ First land created by another person added! 
 🔹 Moved back starting position to better hint at unclaimed areas
 🔹 Moved prop text to center of the screen when it shows
 🔹 Tweaked rotation amount to ensure even degrees (90, 180, 270 are now possible)
@@ -70,7 +70,7 @@ export const changeLog = `
 
 🗓️ May 16, 2021
 🔹 Added prop BUSH and reworked prop controls
-🔹 Fixed a bug where props did not always render 🐛
+🐛 Fixed a bug where props did not always render
 🔹 Added a message if you try to claim a land owned by someone else
 🔹 Added lighting to props
 🔹 Fixed camera so it doesn't go haywire when looking completely up/down
@@ -78,12 +78,12 @@ export const changeLog = `
 
 🗓️ May 15, 2021
 🔹 Improved welcome land and tweaked gravity mode
-🔹 Added a welcome land ⭐
+⭐ Added a welcome land
 🔹 Can now toggle help text
 🔹 Next up is data loading!
 🔹 Mostly code cleanup and restructuring
 🔹 Change log added to start screen
 🔹 Added some fancy page loading and transitions for a more pleasing feeling
-🔹 Prototype client is almost done, should give a feel of what is to come 🚀
+🚀 Prototype client is almost done, should give a feel of what is to come
 
 `.trim();
